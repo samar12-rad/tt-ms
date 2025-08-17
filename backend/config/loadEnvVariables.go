d75982890c9d434c7be3e3a2628479ea8eae772a
samar12-rad <v.samarth1212@gmail.com>
2025-08-17 15:24:20 +0530

    feat: Add .dockerignore and update LoadEnvVariables to handle missing .env file gracefully

--- backend/config/loadEnvVariables.go
+++ backend/config/loadEnvVariables.go
@@ -1,14 +1,15 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Warning: .env file not found, using system environment variables")
 	}
 }
